d0aa8208d6f8b3531572417da4a9973d5f235d95
Szerencsés Bálint <szerenbalint18@gmail.com>
2023-07-23 22:06:47 +0200

    lassan megvan

--- calculate.py
+++ calculate.py
@@ -2,6 +2,8 @@
 nev = input("Add meg a nevedet : ")
 print(f" Üdvözöllek {nev} , hadd mutassam be az én kis játékomat számodra ")
 
+
+
 config = {
 
     "szamitások": [
@@ -13,45 +15,76 @@ config = {
         "6. osszetett müveletek",
         "7. Exit"
     ],
-
-    
-
 }
-
-def is_number(value):
-    try:
-        int(value)  
-        return True
-    except ValueError:
-        return False
-
 
 
 def kivonas():
-    user_input = input("Kérlek, add meg az inputot: ")
-    numbers = [int(num) for num in user_input.split("-")]
-    operations = "-"
+    user_input= input("Kérlek adj meg 2 számot: ")
+    numbers = int(user_input)
     
-    pass
+    return f" A megoldás {numbers[0] + numbers[1]}"
+    return f" A megoldás {numbers[1] + numbers[0]}"
 
 def összeadas():
-    pass
+    user_input1 = input("Kérlek adj meg 2 számot: ")
+    numbers = int(user_input1)
+    
+    return f" A megoldás {numbers[0] + numbers[1]}"
+    return f" A megoldás {numbers[1] + numbers[0]}"
 
 def szorzas():
-    pass
+    user_input2 = input("Kérlek adj meg 2 számot: ")
+    numbers = int(user_input1)
+    
+    return f" A megoldás {numbers[0] + numbers[1]}"
+    return f" A megoldás {numbers[1] + numbers[0]}"
 
 def osztas():
-    pass
+    user_input2 = input("Kérlek adj meg 2 számot: ")
+    numbers = int(user_input2)
+    
+    return f" A megoldás {numbers[0] / numbers[1]}"
+    return f" A megoldás {numbers[1] / numbers[0]}"
 
 def negyzet():
-    pass
+    user_input3 = input("Kérlek adj meg 2 számot: ")
+    numbers = int(user_input3)
+    
+    return f" A megoldás {square(numbers[0])}"
+    return f" A megoldás {square(numbers[1])}"
+    
 
 def osszetett_muveletek():
-    pass
+    pass     
+    
 
 
 def main():
-    pass
+    while True:
+        print("\nMenu:")
+        for item in config['szamitások']:
+            print(item)
+
+        choice = input("Válaszd ki 1-7 :  ")
+
+        if choice == '1':
+            kivonas()
+        elif choice == '2':
+            összeadas()
+        elif choice == '3':
+            szorzas()
+        elif choice == '4':
+            osztas()
+        elif choice == '5':
+            negyzet()
+        elif choice == '6':
+            osszetett_muveletek()
+        elif choice == '7':
+            print("Remélem jól érezted magadat {nev}")
+            break
+        else:
+            print("Invalid choice. Please choose again.")
+    
     
     
 if __name__ == "__main__":
